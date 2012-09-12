e937b420100ccb8b48ab0ed35f433a285f6ac646
Grant James <grantajames@gmail.com>
2012-09-12 15:35:14 -0400

    in progress parseScenariostatements

--- CukeRipper/src/northwoods/cukeripper/utils/parsing/FeatureFileParser.java
+++ CukeRipper/src/northwoods/cukeripper/utils/parsing/FeatureFileParser.java
@@ -2,12 +2,14 @@ package northwoods.cukeripper.utils.parsing;
 
 import java.io.File;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 import northwoods.cukeripper.utils.CommonRips;
 import northwoods.cukeripper.utils.CukeFeature;
 import northwoods.cukeripper.utils.CukeFileReader;
 import northwoods.cukeripper.utils.CukeScenario;
+import northwoods.cukeripper.utils.GWTStatement.StatementType;
 
 public class FeatureFileParser {
 
@@ -53,16 +55,54 @@ public class FeatureFileParser {
 		CukeScenario scenario = new CukeScenario(scenarioName);
 		//
 		System.out.println("~~" + thisCharIndex + "~~");
-		if (index + 1 == indicesOfScenarioTags.size()) {
-			System.out.println(featureContents.substring(thisCharIndex));
-		} else {
-			int nextCharIndex = indicesOfScenarioTags.get(index + 1);
-			System.out.println(featureContents.substring(thisCharIndex,
-					nextCharIndex));
+		String fullScenarioString = getFullScenarioString(featureContents,
+				indicesOfScenarioTags, index, thisCharIndex);
+		// System.out.println(fullScenarioString);
+		//
+		//
+		List<Integer> indicesOfStatements = indicesOfOccurances(
+				fullScenarioString, "Given");
+		indicesOfStatements.addAll(indicesOfOccurances(fullScenarioString,
+				"When"));
+		indicesOfStatements.addAll(indicesOfOccurances(fullScenarioString,
+				"Then"));
+		indicesOfStatements.addAll(indicesOfOccurances(fullScenarioString,
+				"And"));
+		Collections.sort(indicesOfStatements);
+		for (int i : indicesOfStatements) {
+			int snippetLength = "And".length();
+			String snippetOfStatement = fullScenarioString.substring(i, i
+					+ snippetLength);
+			System.out.println(snippetOfStatement);
+			StatementType lastType = StatementType.GIVEN;
+			StatementType thisType = null;
+			if (snippetOfStatement.equals("Giv")) {
+				thisType = StatementType.GIVEN;
+			} else if (snippetOfStatement.equals("Whe")) {
+				thisType = StatementType.WHEN;
+			} else if (snippetOfStatement.equals("The")) {
+				thisType = StatementType.THEN;
+			} else if (snippetOfStatement.equals("And")) {
+				thisType = lastType;
+			}
+			lastType = thisType;
+			System.out.println(thisType.name());
 		}
-
 		//
 		return scenario;
+	}
+
+	private String getFullScenarioString(String featureContents,
+			List<Integer> indicesOfScenarioTags, int index, int thisCharIndex) {
+		String fullScenarioString = "";
+		if (index + 1 == indicesOfScenarioTags.size()) {
+			fullScenarioString = featureContents.substring(thisCharIndex);
+		} else {
+			int nextCharIndex = indicesOfScenarioTags.get(index + 1);
+			fullScenarioString = featureContents.substring(thisCharIndex,
+					nextCharIndex);
+		}
+		return fullScenarioString;
 	}
 
 	private List<Integer> indicesOfOccurances(String str, String subStr) {
